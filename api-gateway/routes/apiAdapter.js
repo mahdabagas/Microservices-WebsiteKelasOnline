10878f88f92821559153d8718cd75891ef73071f
Gazee <100186961+mahdabagas@users.noreply.github.com>
2023-04-18 00:02:53 +0700

    Membuat api gateway

---
+++ api-gateway/routes/apiAdapter.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const {TIMEOUT} = process.env;
+
+module.exports = (baseURL) => {
+    return axios.create({
+        baseURL: baseURL,
+        timeout : TIMEOUT,
+    }
+    )
+}
